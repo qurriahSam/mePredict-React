a0f6f031b01de8f5d10a782caf06c5ad0631b532
90150N <mugirethe1@gmail.com>
2022-05-25 01:29:11 +0300

    st: add route to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import MyNav from "./components/MyNav";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-    </Routes>
+    <>
+      <MyNav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
   );
 }
 
