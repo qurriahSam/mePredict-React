e118a423d6580c4e530e7531d72599eaf3543d91
90150N <mugirethe1@gmail.com>
2022-05-25 11:42:36 +0300

    add game route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import MyNav from "./components/MyNav";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import Game from "./components/Game";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </main>
   );
