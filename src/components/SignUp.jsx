67ff08835129a0c7cbfcb6811f8c16b95aad62fd
90150N <mugirethe1@gmail.com>
2022-05-25 10:28:38 +0300

    add signup form jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Input } from "@material-tailwind/react";
+import { Typography, Input, Button } from "@material-tailwind/react";
 
 const SignUp = () => {
   return (
@@ -9,8 +9,27 @@ const SignUp = () => {
       </Typography>
       <div className="mt-8 w-96 mx-auto bg-indigo-500 p-10">
         <form>
-          <Input label="Username" className="mb-4" />
-          <Input label="Username" className="mb-4" />
+          <div className="pb-5">
+            <Typography>Username:</Typography>
+            <Input label="username" type="text" className="mb-4" />
+          </div>
+          <div className="pb-5">
+            <Typography>Email:</Typography>
+            <Input label="email" type="email" className="mb-4" />
+          </div>
+          <div className="pb-5">
+            <Typography>Password:</Typography>
+            <Input label="password" type="password" className="mb-4" />
+          </div>
+          <div className="pb-5">
+            <Typography>Confirm Password:</Typography>
+            <Input label="confirmPassword" type="password" className="mb-4" />
+          </div>
+          <div className="grid">
+            <Button ripple={true} type="submit" className="bg-[#44ff00] px-14 place-self-center">
+              Submit
+            </Button>
+          </div>
         </form>
       </div>
     </div>
