a5a9b993be46035f65e44f2fcf1b3311bad26ef5
90150N <mugirethe1@gmail.com>
2022-05-25 11:41:39 +0300

    change max width

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -3,7 +3,7 @@ import { Typography } from "@material-tailwind/react";
 
 const Games = () => {
   return (
-    <div className="container mx-auto px-6 py-2.5 w-96 flex justify-center bg-white">
+    <div className="container mx-auto px-6 py-2.5 max-w-screen-sm flex justify-center bg-white">
       <Typography className="font-medium">Guyaquail</Typography>
       <img className="w-8" src="https://media.api-sports.io/football/teams/16477.png" />
       <Typography className="font-medium px-3"> - </Typography>
