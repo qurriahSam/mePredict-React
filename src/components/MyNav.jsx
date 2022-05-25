db0562dfb341d250beda7ceeaf853a412dbfdd6a
90150N <mugirethe1@gmail.com>
2022-05-25 03:08:30 +0300

    st: ahange header color and add logo

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Navbar, Typography } from "@material-tailwind/react";
+import logo from "../assets/logo.png";
 
 const MyNav = () => {
   return (
-    <Navbar className="bg-[#40c50f]" fullWidth="true">
+    <Navbar className="bg-[#40c50f]" fullWidth={true}>
       <div className="container flex justify-between items-center">
-        <Typography className="uppercase font-bold">mepredict</Typography>
+        <Typography className="uppercase font-bold flex">
+          <img className="w-14 pr-2" src={logo} />
+          mepredict
+        </Typography>
         <Typography>Login</Typography>
       </div>
     </Navbar>
