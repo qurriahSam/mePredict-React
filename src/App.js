43977da31fe8fa5cb2adc7c9dd9295ae25b84967
90150N <mugirethe1@gmail.com>
2022-05-25 04:11:24 +0300

    add signup page route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import MyNav from "./components/MyNav";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <>
+    <main className="h-screen bg-[#000C1D]">
       <MyNav />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
