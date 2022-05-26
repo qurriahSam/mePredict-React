eaa7b3f96a6f856be86dc15d29d00c790621f976
90150N <mugirethe1@gmail.com>
2022-05-27 01:13:27 +0300

    set like state and colors

--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -1,28 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { Typography } from "@material-tailwind/react";
 
 const Prediction = () => {
+  const [like, setLike] = useState(false);
+
+  const handleLike = () => setLike(!like);
+
   return (
     <>
-      <div className="border-green-500 border-2  p-2 flex align-middle">
+      <div className="border-[#44ff00] border-2  p-2 flex justify-around">
         <div>
-          <FontAwesomeIcon icon={faThumbsUp} className="text-white" />
-        </div>
-
-        <Typography color="white">
-          <span>1</span>
-          <span>-</span>
-          <span>3</span>
-        </Typography>
-      </div>
-      <div className="border-green-500 border-2 p-2 flex space-x-5">
-        <div className="grid grid-cols-2 gap-1">
-          <FontAwesomeIcon icon={faThumbsUp} className="text-white pt-1" />
-          <Typography as="span" color="white">
-            2
-          </Typography>
+          <FontAwesomeIcon
+            icon={faThumbsUp}
+            className={like ? "text-[#44ff00]" : "text-white"}
+            onClick={handleLike}
+          />
+          <span className="text-white pl-2">2</span>
         </div>
 
         <Typography color="white">
