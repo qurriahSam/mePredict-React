99ba1f9521798a1a60f6e2c128c1982785007160
90150N <mugirethe1@gmail.com>
2022-05-26 19:38:30 +0300

    add links to navbar components

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Navbar, Typography } from "@material-tailwind/react";
+import { Navbar, Typography, Button } from "@material-tailwind/react";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const MyNav = () => {
   return (
     <Navbar className="bg-[#44ff00]" fullWidth={true}>
       <div className="container flex justify-between items-center">
-        <Typography className="uppercase font-bold flex">
-          <img className="w-14 pr-2" src={logo} />
-          mepredict
-        </Typography>
-        <Typography>Login</Typography>
+        <Link to="/">
+          <Typography className="uppercase font-bold flex">
+            <img className="w-14 pr-2" src={logo} />
+            mepredict
+          </Typography>
+        </Link>
+        <Link to="/signin">
+          <Button variant="text" className="text-[#fff]" size="md">
+            Login
+          </Button>
+        </Link>
       </div>
     </Navbar>
   );
