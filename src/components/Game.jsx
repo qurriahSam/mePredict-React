1715a1f39fdb973ebd800320b9249b57f5afa7d4
90150N <mugirethe1@gmail.com>
2022-06-27 14:35:39 +0300

    padding and borders set

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -17,13 +17,17 @@ const Game = ({ games }) => {
   console.log(game);
   return (
     <>
-      <div className="container mx-auto px-2">
-        <div className="container mx-auto px-6 py-2.5 max-w-lg mt-24 flex justify-center bg-white">
-          <Typography className="font-medium">{game.teams.home.name}</Typography>
-          <img className="w-8" src={game.teams.home.logo} />
-          <Typography className="font-medium px-3"> - </Typography>
-          <img className="w-8" src={game.teams.away.logo} />
-          <Typography className="font-medium">{game.teams.away.name}</Typography>
+      <div className="container mx-auto px-2 ">
+        <div className="container mx-auto px-6 py-2.5 max-w-lg mt-24 flex justify-center bg-white border-2 border-indigo-600">
+          <div className="flex">
+            <Typography className="font-medium pr-4">{game.teams.home.name}</Typography>
+            <img className="w-8" src={game.teams.home.logo} />
+          </div>
+          <Typography className="font-medium px-4"> - </Typography>
+          <div className="flex">
+            <img className="w-8" src={game.teams.away.logo} />
+            <Typography className="font-medium pl-3">{game.teams.away.name}</Typography>
+          </div>
         </div>
       </div>
 
